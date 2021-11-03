b367693a7a9e9d2f2b11b8d10f81ef2b94dba1a8
aanvarov <anvarov2295@gmail.com>
2021-11-03 16:41:48 +0500

    category class created

---
+++ MicroservicesAPI_10888/Models/Category.cs
@@ -0,0 +1,9 @@
+namespace MicroservicesAPI_10888.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}

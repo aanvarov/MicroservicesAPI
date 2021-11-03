c5d389ce8d8bf0f422d5243af5c27475ee114397
aanvarov <anvarov2295@gmail.com>
2021-11-04 01:37:47 +0500

    price property added to product class

--- MicroservicesAPI_10888/Models/Product.cs
+++ MicroservicesAPI_10888/Models/Product.cs
@@ -5,6 +5,7 @@ namespace MicroservicesAPI_10888.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public decimal Price { get; set; }
         public Category ProductCategory { get; set; }
     }
 }
